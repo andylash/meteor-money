d37b12919e6c5da29f92c901d76e2b0ac77d9a2f
Andy Lash <andy@squidnet.com>
2015-07-29 10:54:04 -0700

    Refactor Money EJSON type registration so static Money is available

--- src/common/ejson_types.js
+++ src/common/ejson_types.js
@@ -4,6 +4,5 @@
 //Even though this call exists in the money-core npm library, we need to call it again
 //as under the covers this is building a list of types in memory
 var MONEY_TYPE_NAME = 'Money';
-EJSON.addType(MONEY_TYPE_NAME, function(m) {
-  return new Money(m.amount, m.currency, true);
-});
+
+EJSON.addType(MONEY_TYPE_NAME, Money.fromJSONValue);
